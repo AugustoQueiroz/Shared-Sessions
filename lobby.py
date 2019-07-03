3f96be49bfe5f5a751800ec999dc7ce7f184ead6
Igor Moura <imp2@cin.ufpe.br>
2019-07-03 15:10:03 -0300

    Modified root request for GET instead of POST

--- lobby.py
+++ lobby.py
@@ -26,27 +26,23 @@ sp = spotipy.Spotify()
 
 @app.route("/", methods=["GET", "POST"])
 def user_in():
-    if request.method == "POST":
 
-        if "user_id" not in request.form:
-            result = {"sucess": False, "error": "Missing user_id parameter"}
-            return json.dumps(result)
-
-        user_id = request.form["user_id"]
-
-        if user_id == "":
-            result = {"sucess": False, "error": "Empty user_id"}
-            return json.dumps(result)
-
-        return "hi"  # TODO: return template html page
-
-    else:
-        result = {"sucess": False, "error": "invalid request. POST only"}
+    user_id = request.args.get("user_id", "")
+    if user_id == "":
+        result = {"sucess": False, "error": "Empty user_id"}
         return json.dumps(result)
+
+    return render_template("index.html", user=user_id)
 
 
 @app.route("/joinSession/", methods=["GET", "POST"])
 def join_session():  # TODO: update user room with newly joined room
+
+    user_id = request.args.get("user_id", "")
+    if user_id == "":
+        result = {"sucess": False, "error": "Empty user_id"}
+        return json.dumps(result)
+
     session_id = request.args.get("sessionCode", "")
     if session_id == "":
         result = {"sucess": False, "error": "Empty session_id"}
