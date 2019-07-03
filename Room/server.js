61ba15417e9a15bac96a190779226306d87415f9
Ladson <lgs3@cin.ufpe.br>
2019-07-03 15:49:29 -0300

    loop and docker set up

--- Room/server.js
+++ Room/server.js
@@ -1,8 +1,7 @@
 var firebase = require("firebase");
 var SpotifyWebApi = require('spotify-web-api-node');
-var request = require('request');
 
-var roomToken = '1'
+var roomToken = process.env.token_room || '1';
 
 var firebaseConfig = {
   apiKey: "AIzaSyAowSk2cCFSnfBCqjUOW_SPOokjjNjvyoo",
@@ -18,51 +17,46 @@ var defaultProj = firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-/*database.ref('users/' + userId).set({
-  username: 'Ladson',
-  email: 'blabla',
-}, function(error) {
-  if (error) {
-    // The write failed...
-    console.log('bad');
-  } else {
-    // Data saved successfully!
-    console.log('good');
-  }
-});
-*/
-var tokens;
-database.ref('rooms/' + roomToken).once('value').then(function(snap) {
-  tokens = snap.val().users;
-  var spotifyApi = new SpotifyWebApi({
-    redirectUri: '0.0.0.0:8000',
-    clientId: '32cca9f1a35c44d4bed142d7fe78a3a8'
-  })
-  console.log(tokens[0]);
-  
-  spotifyApi.setAccessToken(tokens[0]);
-  spotifyApi.getMyCurrentPlaybackState({
-  })
-  .then(function(data) {
-    // Output items
-    console.log(data.body.item.uri);
-    console.log(data.body.progress_ms);
+var master = {};
 
-    console.log("Now Playing: ",data.body);
-    const payload = {"context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
-    "offset": {
-      "position": 4
-    },
-    "position_ms": 20000
-  };
-    spotifyApi.play(payload)//({context_uri:'spotify:track:0CZ8lquoTX2Dkg7Ak2inwA',offset:5, position_ms:'199978'})
-    .then(function(yay){
-      //whatever
-      console.log(yay);
-    }, function(err){
-      console.log(err);
-    });
-  }, function(err) {
-    console.log('Something went wrong!', err);
+var intervalId = setInterval(function () {
+  database.ref('rooms/' + roomToken).once('value').then(function (snap) {
+    tokens = snap.val().users;
+    var spotifyApi = new SpotifyWebApi({
+      redirectUri: '0.0.0.0:8000',
+      clientId: '32cca9f1a35c44d4bed142d7fe78a3a8'
+    })
+    //console.log(tokens[0]);
+  
+    for (i = 0; i < tokens.length; i++) {
+  
+      spotifyApi.setAccessToken(tokens[i]);
+      spotifyApi.getMyCurrentPlaybackState({
+      })
+        .then(function (data) {
+          // Output items
+          console.log(data.body.item.uri);
+          console.log(data.body.progress_ms);
+
+          console.log("Now Playing: ", data.body);
+          const payload = {
+            "context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
+            "offset": {
+              "position": 6
+            },
+            "position_ms": 20000
+          };
+          spotifyApi.play(payload)//({context_uri:'spotify:track:0CZ8lquoTX2Dkg7Ak2inwA',offset:5, position_ms:'199978'})
+            .then(function (yay) {
+              //whatever
+              console.log(yay);
+            }, function (err) {
+              console.log(err);
+            });
+        }, function (err) {
+          console.log('Something went wrong!', err);
+        });
+    }
+
   });
-});
+}, 5000);
